2634fa08c90aeeb38b7b90b74f3e445916456c5b
GrigoDev <grigodev@yahoo.com>
2025-08-17 14:39:46 +0300

    Pattern repository

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -1,19 +1,26 @@
 package link
 
 import (
+	"fmt"
 	"net/http"
 )
 
-type LinkHandlerDeps struct{}
+type LinkHandlerDeps struct {
+	LinkRepository *LinkRepository
+}
 
-type LinkHandler struct{}
+type LinkHandler struct {
+	LinkRepository *LinkRepository
+}
 
 func NewLinkHandler(router *http.ServeMux, deps LinkHandlerDeps) {
-	handler := &LinkHandler{}
+	handler := &LinkHandler{
+		LinkRepository: deps.LinkRepository,
+	}
 	router.HandleFunc("POST /link", handler.Create())
 	router.HandleFunc("PATCH /link/{id}", handler.Update())
 	router.HandleFunc("DELETE /link/{id}", handler.Delete())
-	router.HandleFunc("GET /{alias}", handler.GoTo())
+	router.HandleFunc("GET /{hash}", handler.GoTo())
 }
 
 func (handler *LinkHandler) Create() http.HandlerFunc {
@@ -30,7 +37,8 @@ func (handler *LinkHandler) Update() http.HandlerFunc {
 
 func (handler *LinkHandler) Delete() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		id := r.PathValue("id")
+		fmt.Println(id)
 	}
 }
 
---
+++ internal/link/repository.go
@@ -0,0 +1,19 @@
+package link
+
+import (
+	"github.com/GrigoDev/linker/pkg/db"
+)
+
+type LinkRepository struct {
+	Database *db.Db
+}
+
+func NewLinkRepository(database *db.Db) *LinkRepository {
+	return &LinkRepository{
+		Database: database,
+	}
+}
+
+func (repo *LinkRepository) Create(link *Link) {
+
+}
