ed7c71652bedf604c050fd353a7d0b7ff55a19cc
GrigoDev <grigodev@yahoo.com>
2025-09-01 22:58:24 +0300

    Service auth

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/GrigoDev/linker/configs"
 	"github.com/GrigoDev/linker/internal/auth"
 	"github.com/GrigoDev/linker/internal/link"
+	"github.com/GrigoDev/linker/internal/user"
 	"github.com/GrigoDev/linker/pkg/db"
 	"github.com/GrigoDev/linker/pkg/middleware"
 )
@@ -18,10 +19,15 @@ func main() {
 
 	// Repositories
 	LinkRepository := link.NewLinkRepository(db)
+	userRepository := user.NewUserRepository(db)
+
+	// Services
+	authService := auth.NewAuthService(userRepository)
 
 	// Hanlder
 	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
-		Config: conf,
+		Config:      conf,
+		AuthService: authService,
 	})
 	link.NewLinkHandler(router, link.LinkHandlerDeps{
 		LinkRepository: LinkRepository,
