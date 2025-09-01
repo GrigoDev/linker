b18a995be71f18c0fd4903b8f57d9e0ff28af306
GrigoDev <grigodev@yahoo.com>
2025-09-01 22:17:48 +0300

    Add UserRepository

---
+++ internal/user/repository.go
@@ -0,0 +1,34 @@
+package user
+
+import "github.com/GrigoDev/linker/pkg/db"
+
+// Create
+
+// Find by email
+
+// по аналогии с линком
+
+type UserRepository struct {
+	database *db.Db
+}
+
+func NewUserRepository(database *db.Db) *UserRepository {
+	return &UserRepository{database: database}
+}
+
+func (repo *UserRepository) Create(user *User) (*User, error) {
+	result := repo.database.DB.Create(user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return user, nil
+}
+
+func (repo *UserRepository) FindByEmail(email string) (*User, error) {
+	var user User
+	result := repo.database.DB.First(&user, "email = ?", email)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
+}
